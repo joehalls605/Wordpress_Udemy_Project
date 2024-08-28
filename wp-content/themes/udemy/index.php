3d3155c3442b73dc9bfd7ac815abee3caf0771a9
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-08-28 11:46:44 +0100

    working with theme.json

--- wp-content/themes/udemy/index.php
+++ wp-content/themes/udemy/index.php
@@ -1,16 +1,16 @@
+<!-- this was for learning, no longer used at the moment because doing full site editing-->
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes();?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo("charset");?>">
+    <?php wp_head(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        :lang(ar){
-            direction:rtl;
-        }
-    </style>
 </head>
-<body>
+<body <?php body_class("example"); ?>>
     <p>Hello World!</p>
+    <?php wp_body_open(); ?>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
