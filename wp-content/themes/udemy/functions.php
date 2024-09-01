1844d31c5e6187bcba4837f14a15dd1eabb75b55
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-09-01 15:25:01 +0100

    fixed the styling issue

--- wp-content/themes/udemy/functions.php
+++ wp-content/themes/udemy/functions.php
@@ -1,15 +1,14 @@
 <?php
 
-// Variables
 
 
-// Includes
-include(get_theme_file_path("/includes/front/enqueue.php"));
+//Variables
 
-// Hooks
 
- // add_action tells wp to run a function when a specific hook runs
- add_action("wp_enqueue_scripts", "u_enqueue");
+//Includes
+include(get_template_directory() .'/includes/front/enqueue.php');
 
-// wp_enqueue_scripts is a WordPress hook that is used to add custom scripts and styles to the front end of the site.
-// u_enqueue is the function that will be called when wp_enqueue_scripts runs. It contains the actual code to load the scripts and styles.
+//Hooks
+
+add_action('wp_enqueue_scripts', 'u_enqueue');
+ 
